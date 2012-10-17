0ecd73e5e384a77aba0158884f26469cfe9a2504
Jim Paterson <James.Paterson@gcu.ac.uk>
2012-10-17 23:16:25 +0100

    initial commit

---
+++ PartialViewDemo/PartialViewDemo/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartialViewDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Partial view example";
+
+            return View();
+        }
+
+        public ActionResult Ajax()
+        {
+            ViewBag.Message = "Partial view with Ajax example";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Content()
+        {
+            ViewBag.Content = "Here is some content";
+
+            return PartialView();
+        }
+    }
+}
